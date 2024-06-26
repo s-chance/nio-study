f0222a943395e665b82611c4320d0db0bfa5b261
s-chance <entropy.tree@outlook.com>
2024-06-26 17:14:03 +0800

    feat(*): no-blocking io

--- nio-bytebuffer/src/test/java/org/entropy/network/Server.java
+++ nio-bytebuffer/src/test/java/org/entropy/network/Server.java
@@ -14,13 +14,14 @@ import java.util.List;
 @Slf4j
 public class Server {
     public static void main(String[] args) throws IOException {
-        // 使用 nio 来理解阻塞模式，单线程
+        // 使用 nio 来理解阻塞模式/非阻塞模式，单线程
 
         // 0. ByteBuffer
         ByteBuffer buffer = ByteBuffer.allocate(16);
 
         // 1. 创建服务端
         ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false); // 非阻塞模式
 
         // 2. 绑定监听端口
         ssc.bind(new InetSocketAddress(8080));
@@ -29,18 +30,21 @@ public class Server {
         List<SocketChannel> channels = new ArrayList<>();
         while (true) {
             // 4. accept 建立与客户端的连接，SocketChannel 用来与客户端之间通信
-            log.debug("connecting...");
-            SocketChannel sc = ssc.accept(); // 阻塞方法，线程停止运行
-            log.debug("connected... {}", sc);
-            channels.add(sc);
+            SocketChannel sc = ssc.accept(); // 非阻塞，线程还会继续运行，如果没有连接建立，sc 为 null
+            if (sc != null) {
+                log.debug("connected... {}", sc);
+                sc.configureBlocking(false); // 非阻塞模式
+                channels.add(sc);
+            }
             for (SocketChannel channel : channels) {
                 // 5. 接收客户端发送的数据
-                log.debug("before read... {}", channel);
-                channel.read(buffer); // 阻塞方法，线程停止运行
-                buffer.flip();
-                ByteBufferUtil.debugAll(buffer);
-                buffer.clear();
-                log.debug("after read... {}", channel);
+                int read = channel.read(buffer);// 非阻塞，线程仍然会继续运行，如果没有读到数据，read 返回 0
+                if (read > 0) {
+                    buffer.flip();
+                    ByteBufferUtil.debugAll(buffer);
+                    buffer.clear();
+                    log.debug("after read... {}", channel);
+                }
             }
         }
     }
